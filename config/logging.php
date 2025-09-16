7789d442214990669c25129f375e94eb61d5cb83
Eyas T <eyasthaha@gmail.com>
2025-09-16 14:13:32 +0530

    Added - logging api requests

--- config/logging.php
+++ config/logging.php
@@ -52,6 +52,12 @@ return [
 
     'channels' => [
 
+        'custom' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/requests.log'),
+            'level' => 'info',
+        ],
+
         'stack' => [
             'driver' => 'stack',
             'channels' => explode(',', (string) env('LOG_STACK', 'single')),
